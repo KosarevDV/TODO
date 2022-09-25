1c11a4c6479cf99266c3f7ff715551782b981fe7
KosarevDV <KosarevDV@users.noreply.github.com>
2022-09-25 17:53:27 +0300

    ДЗ №4 - требует доработки

--- todo_project/todo/views.py
+++ todo_project/todo/views.py
@@ -1,14 +1,37 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
-from todo.models import Project, TODO
+from .models import Project, TODO
 from todo.serializers import ProjectModelSerializer, TODOModelSerializer
+
+from .filters import ProjectFilter
+
+
+class ProjectLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 10
+
+
+class TODOLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 20
 
 
 class ProjectModelViewSet(ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectModelSerializer
+    pagination_class = ProjectLimitOffsetPagination
+    filterset_fields = ProjectFilter
 
 
-class TODOModelViewSet(ModelViewSet):
+class TODOModelViewSet(APIView):
     queryset = TODO.objects.all()
     serializer_class = TODOModelSerializer
+    pagination_class = TODOLimitOffsetPagination
+    filterset_fields = ['name']
+
+    def __delete__(self, request, pk):
+        instance = self.get_object(pk=pk)
+        instance.is_active = False
+        return Response(status=status.HTTP_204_NO_CONTENT)
